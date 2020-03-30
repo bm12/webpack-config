4e42911fd753243b154385fed594d78edd881ff1
Babaev Mahmoud <mahmud1997babaev@gmail.com>
2020-03-31 00:15:11 +0300

    Add css loaders

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { log } from './utils/log';
+import './styles.css';
 
 log('Hello, world!');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
 
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = process.env.NODE_ENV === 'production';
+
+const cssLoaders = () => {
+  const loaders = [];
+
+  if (isDev) {
+    loaders.push('style-loader')
+  } else {
+    loaders.push({
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+      },
+    });
+  }
+
+  loaders.push('css-loader');
+
+  return loaders
+};
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
@@ -36,6 +56,15 @@ module.exports = {
       minify: {
         collapseWhitespace: true,
       }
-    })
+    }),
+    new MiniCssExtractPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders(),
+      },
+    ],
+  },
 };
